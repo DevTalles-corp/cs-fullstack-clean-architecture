41bcb175c76a80e0d902082f981628073fcec11a
Teddy Paz <pazteddy@gmail.com>
2025-09-01 18:52:54 -0400

    Implement GetNotesQuery and GetNotesQueryHandler for retrieving notes

---
+++ TechNotes.Application/Notes/GetNotes/GetNotesQueryHandler.cs
@@ -0,0 +1,19 @@
+using System;
+using MediatR;
+using TechNotes.Domain.Notes;
+
+namespace TechNotes.Application.Notes.GetNotes;
+
+public class GetNotesQueryHandler : IRequestHandler<GetNotesQuery, List<Note>>
+{
+  private readonly INoteRepository _noteRepository;
+
+  public GetNotesQueryHandler(INoteRepository noteRepository)
+  {
+    _noteRepository = noteRepository;
+  }
+  public async Task<List<Note>> Handle(GetNotesQuery request, CancellationToken cancellationToken)
+  {
+    return await _noteRepository.GetAllNotesAsync();
+  }
+}
