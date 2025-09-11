8a3b4a38af88d7448813853f8c6a055cfdcf53b1
Teddy Paz <pazteddy@gmail.com>
2025-09-11 16:15:37 -0400

    Refactor Result class to include generic Result<T> for handling typed operation outcomes

--- TechNotes.Domain/Abtractions/Result.cs
+++ TechNotes.Domain/Abtractions/Result.cs
@@ -16,4 +16,15 @@ public class Result
 
   public static Result Success() => new(true);
   public static Result Failure(string errorMessage) => new(false, errorMessage);
+
+  public static Result<T> Ok<T>(T? value) => new(value, true, null);
+  public static Result<T> Fail<T>(string errorMessage) => new(default, false, errorMessage);
+}
+public class Result<T> : Result
+{
+  public T? Value { get; }
+  protected internal Result(T? value, bool isSuccessful, string? errorMessage) : base(isSuccessful, errorMessage)
+  {
+    Value = value;
+  }
 }
