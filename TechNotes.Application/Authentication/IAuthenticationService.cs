82dbf603740d42507cd90ffde0909b05bfb3140a
Teddy Paz <pazteddy@gmail.com>
2025-09-24 19:18:16 -0400

    feat: Implement IAuthenticationService interface and RegisterUserResponse class for user registration and authentication

---
+++ TechNotes.Application/Authentication/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TechNotes.Application.Authentication;
+
+public interface IAuthenticationService
+{
+  Task<RegisterUserResponse> RegisterUserAsync(string userName, string email, string password);
+  Task<bool> LoginUserAsync(string email, string password);
+
+}
