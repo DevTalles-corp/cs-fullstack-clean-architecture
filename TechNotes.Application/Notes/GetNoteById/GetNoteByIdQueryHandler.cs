77349d00bcb3ecdf9e83b630e594a513d01e4b10
Teddy Paz <pazteddy@gmail.com>
2025-10-14 11:56:31 -0400

    feat: Add edit link to note view for authorized users and include user ID in note response

--- TechNotes.Application/Notes/GetNoteById/GetNoteByIdQueryHandler.cs
+++ TechNotes.Application/Notes/GetNoteById/GetNoteByIdQueryHandler.cs
@@ -1,3 +1,4 @@
+using TechNotes.Application.Users;
 using TechNotes.Domain.User;
 
 namespace TechNotes.Application.Notes.GetNoteById;
@@ -6,10 +7,12 @@ public class GetNoteByIdQueryHandler : IQueryHandler<GetNoteByIdQuery, NoteRespo
 {
   private readonly INoteRepository _noteRepository;
   private readonly IUserRepository _userRepository;
-  public GetNoteByIdQueryHandler(INoteRepository noteRepository, IUserRepository userRepository)
+  private readonly IUserService _userService;
+  public GetNoteByIdQueryHandler(INoteRepository noteRepository, IUserRepository userRepository, IUserService userService)
   {
     _noteRepository = noteRepository;
     _userRepository = userRepository;
+    _userService = userService;
   }
   public async Task<Result<NoteResponse?>> Handle(GetNoteByIdQuery request, CancellationToken cancellationToken)
   {
@@ -23,6 +26,8 @@ public class GetNoteByIdQueryHandler : IQueryHandler<GetNoteByIdQuery, NoteRespo
     {
       var noteAuthor = await _userRepository.GetUserByIdAsync(note.UserId);
       noteResponse.UserName = noteAuthor?.UserName ?? "Desconocido";
+      noteResponse.UserId = note.UserId;
+      noteResponse.CanEdit = await _userService.CurrentUserCanEditNoteAsync(note.Id);
     }
     else
     {
