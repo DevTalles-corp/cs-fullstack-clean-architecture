4af11c50c0ea24d51300dd40a56df5368a2ea5a5
Teddy Paz <pazteddy@gmail.com>
2025-10-23 15:11:21 -0400

    feat: Refactor NoteOverview component to use NotesOverviewServiceClient and add TogglePublishNote functionality

---
+++ TechNotes.Client/Features/Notes/NotesOverviewServiceClient.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net.Http.Json;
+using TechNotes.Application.Notes;
+
+namespace TechNotes.Client.Features.Notes;
+
+public class NotesOverviewServiceClient : INotesOverviewService
+{
+  private readonly HttpClient _http;
+
+  public NotesOverviewServiceClient(HttpClient http)
+  {
+    _http = http;
+  }
+  public async Task<List<NoteResponse>?> GetNoteByCurrentUserAsync()
+  {
+    return await _http.GetFromJsonAsync<List<NoteResponse>>("/api/notes");
+  }
+
+  public async Task<NoteResponse?> TogglePublishNoteAsync(int NoteId)
+  {
+    var result = await _http.PatchAsync($"api/notes/{NoteId}", null);
+    if (result is not null && result.Content is not null)
+    {
+      return await result.Content.ReadFromJsonAsync<NoteResponse>();
+    }
+    return null;
+  }
+}
